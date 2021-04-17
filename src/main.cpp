8e6e32396ee16f7b05a902765805a10d1f373da5
Wisnu Mulya <wisnu@wisnumulya.com>
2021-04-17 16:37:02 +0700

    Add feature: car driving itself well on starting lane

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
 
 // for convenience
 using nlohmann::json;
@@ -49,9 +50,15 @@ int main() {
     map_waypoints_dx.push_back(d_x);
     map_waypoints_dy.push_back(d_y);
   }
+  
+  // Ego car starts at lane 1
+  int lane = 1;
+
+  // Set reference speed for the car
+  double ref_velocity = 0.0; // MPH
 
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
-               &map_waypoints_dx,&map_waypoints_dy]
+               &map_waypoints_dx,&map_waypoints_dy,&lane,&ref_velocity]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -89,15 +96,152 @@ int main() {
           auto sensor_fusion = j[1]["sensor_fusion"];
 
           json msgJson;
-
-          vector<double> next_x_vals;
-          vector<double> next_y_vals;
-
+ 
           /**
            * TODO: define a path made up of (x,y) points that the car will visit
            *   sequentially every .02 seconds
            */
+	  // Previous path size
+	  int previous_path_size = previous_path_x.size();
 
+	  // Check if there's a car too close to the ego car
+	  bool is_too_close = false;
+	  double sf_s, sf_d, sf_vx, sf_vy, sf_velocity, s_future;
+
+	  for (int i = 0; i < sensor_fusion.size(); i++) {
+	    // Check if a car is in ego car's lane
+	    sf_d = sensor_fusion[i][6];
+
+	    if ((sf_d < 2 + (lane * 4) + 2) && (sf_d > 2 + (lane * 4) - 2)) {
+	      sf_vx = sensor_fusion[i][3];
+	      sf_vy = sensor_fusion[i][4];
+	      sf_velocity = sqrt(pow(sf_vx, 2) + pow(sf_vy, 2));
+	      sf_s = sensor_fusion[i][5];
+
+	      // Check if a car's future position would be too close to the ego car's end path waypoint
+	      s_future = sf_s + (previous_path_size * .02 * sf_velocity);
+
+	      if ((s_future > end_path_s) && (s_future - end_path_s < 30)) {
+		is_too_close = true;
+	      }
+	    }
+	  }
+
+	  // Define anchor points for appending  waypoints to the next path
+	  vector<double> anchor_points_x, anchor_points_y;
+
+	  // Define reference points as either where the car is or the end point of previous path
+	  double ref_x = car_x;
+	  double ref_y = car_y;
+	  double ref_yaw = deg2rad(car_yaw);
+
+	  // If previous path is almost empty, set car's location as reference
+	  if (previous_path_size < 3) {
+	    double previous_car_x = car_x - cos(car_yaw);
+	    double previous_car_y = car_y - sin(car_yaw);
+
+	    anchor_points_x.push_back(previous_car_x);
+	    anchor_points_x.push_back(car_x);
+
+	    anchor_points_y.push_back(previous_car_y);
+	    anchor_points_y.push_back(car_y);
+	  } else {
+	    ref_x = previous_path_x[previous_path_size - 1];
+	    ref_y = previous_path_y[previous_path_size - 1];
+
+	    double ref_x_previous = previous_path_x[previous_path_size - 2];
+	    double ref_y_previous = previous_path_y[previous_path_size - 2];
+	    ref_yaw = atan2(ref_y - ref_y_previous, ref_x - ref_x_previous);
+
+	    anchor_points_x.push_back(ref_x_previous);
+	    anchor_points_x.push_back(ref_x);
+
+	    anchor_points_y.push_back(ref_y_previous);
+	    anchor_points_y.push_back(ref_y);
+	  }
+
+	  // Append anchor waypoints with 30m space in between them
+	  vector<double> anchor_wp0 = getXY(car_s + 30, (2 + 4 * lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+	  vector<double> anchor_wp1 = getXY(car_s + 60, (2 + 4 * lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+	  vector<double> anchor_wp2 = getXY(car_s + 90, (2 + 4 * lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+	  anchor_points_x.push_back(anchor_wp0[0]);
+	  anchor_points_x.push_back(anchor_wp1[0]);
+	  anchor_points_x.push_back(anchor_wp2[0]);
+
+	  anchor_points_y.push_back(anchor_wp0[1]);
+	  anchor_points_y.push_back(anchor_wp1[1]);
+	  anchor_points_y.push_back(anchor_wp2[1]);
+
+
+
+	  // Transform anchor points' basis, so the car is at origin and at 0 degree yaw
+	  double shift_x, shift_y;
+	  for (int i = 0; i < anchor_points_x.size(); i++) {
+	    shift_x = anchor_points_x[i] - ref_x;
+	    shift_y = anchor_points_y[i] - ref_y;
+
+	    anchor_points_x[i] = shift_x * cos(-ref_yaw) - shift_y * sin(-ref_yaw);
+	    anchor_points_y[i] = shift_x * sin(-ref_yaw) + shift_y * cos(-ref_yaw);
+	  }
+	  
+	  // Create a spline
+	  tk::spline spline;
+	  // Set points to the spline
+	  spline.set_points(anchor_points_x, anchor_points_y);
+	  
+	  // The car's path
+	  vector<double> next_x_vals;
+          vector<double> next_y_vals;
+
+	  // Append all previous path to the next path
+	  for (int i = 0; i < previous_path_size; i++) {
+	    next_x_vals.push_back(previous_path_x[i]);
+	    next_y_vals.push_back(previous_path_y[i]);
+	  }
+
+	  // Calculate the number of points the spline would be splitted to achieve the target velocity
+	  double target_x = 30.0;
+	  double target_y = spline(target_x);
+	  double target_distance = sqrt(pow(target_x, 2) + pow(target_y, 2));
+	  double target_velocity = 49.5;
+	  
+	  const double meter_per_mile_ = 1609.344;
+	  const double seconds_per_hour_ = 3600.0;;
+
+	  // Define x_add_on as waypoint x to add in the basis of car's at origin with yaw 0
+	  double x_add_on = 0;
+
+	  // Adding waypoints to the next path
+	  double N, next_x, next_y;
+	  
+	  for (int i = 0; i < 50 - previous_path_size; ++i) {
+	    // Slows down if there's a car too close or speeds up otherwise
+	    if (is_too_close) {
+	      ref_velocity -= .224;
+	    } else if (ref_velocity < target_velocity) {
+	      ref_velocity += .224;
+	    }
+
+	    // Set N for the number of points to take in a splie
+	    N = target_distance / (0.02 * (ref_velocity / (seconds_per_hour_ / meter_per_mile_)));
+	    
+	    next_x = x_add_on + (target_x / N);
+	    next_y = spline(next_x);
+
+	    x_add_on = next_x;
+
+	    // Transform next X and Y to the basis of the map
+	    shift_x = next_x;
+	    shift_y = next_y;
+
+	    next_x = ref_x + (shift_x * cos(ref_yaw) - shift_y * sin(ref_yaw));
+	    next_y = ref_y + (shift_x * sin(ref_yaw) + shift_y * cos(ref_yaw));
+	    
+	    next_x_vals.push_back(next_x);
+	    next_y_vals.push_back(next_y);
+	  }
+	  // END
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
